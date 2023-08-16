9391a9beee22f7a66ecea4f900206f1381f90f38
ilhamsabana900 <ilhamsabana900@gmail.com>
2023-08-16 22:39:00 +0700

    membuat rest-api get&post menggunakan postman

---
+++ database/migrations/2023_08_16_142113_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Testing\Constraints\SoftDeletedInDatabase;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('news_content');
+            $table->unsignedBigInteger('author');
+            $table->timestamps();
+            $table->SoftDeletes();
+
+            $table->foreign('author')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2023_08_16_142755_create_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('posts_id');
+            $table->unsignedBigInteger('users_id');
+            $table->text('comments_contens');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('posts_id')->references('id')->on('posts');
+            $table->foreign('users_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};

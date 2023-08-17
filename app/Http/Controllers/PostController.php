edc46556d05c4e697c5c96f40bf93c00eebd4f8c
ilhamsabana900 <ilhamsabana900@gmail.com>
2023-08-17 21:28:46 +0700

    api resource \, eager loading,belongto

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,12 +5,22 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\PostDetailResource;
 
 class PostController extends Controller
 {
     public function index(){
         $posts= Post::all();
-        return response()->json($posts);
+        return PostResource::collection($posts);
+    }
+    public function show($id){
+        $post= Post::with('writer:id,username')->FindOrFail($id);
+        return new PostDetailResource($post);
+    }
+    public function show2($id){
+        $post= Post::FindOrFail($id);
+        return new PostDetailResource($post);
     }
 }
  
